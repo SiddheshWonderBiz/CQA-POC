f0d50cda0728657a49dda54cc614fcbfaaecf747
SiddheshWonderBiz <siddhesh.wayal@wonderbiz.in>
2025-12-19 15:36:15 +0530

    Project Setup

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import '@mescius/spread-sheets/styles/gc.spread.sheets.excel2013white.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
