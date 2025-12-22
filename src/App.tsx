221ff50090bfe92bf5cab85689bf1a6f6b5890b7
SiddheshWonderBiz <siddhesh.wayal@wonderbiz.in>
2025-12-22 10:29:30 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
+import Spreadsheet from "./components/spreadsheet/Spreadsheet"
 
-import { SpreadSheets } from '@mescius/spread-sheets-react'
 
 function App() {
-
   return (
-    <>
-    <div style={{ width: '100%', height: '100%' }}>
-      <SpreadSheets />
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Spreadsheet />
     </div>
-    </>
   )
 }
 
