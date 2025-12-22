680b9e62f54f3697120bda3cbaa1f9f59c54f7b0
SiddheshWonderBiz <siddhesh.wayal@wonderbiz.in>
2025-12-22 09:32:33 +0530

    my component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './index.css'
-import '@mescius/spread-sheets/styles/gc.spread.sheets.excel2013white.css';
-
+import '@mescius/spread-sheets/styles/gc.spread.sheets.excel2013white.css'
 
 ReactDOM.render(
   <React.StrictMode>
