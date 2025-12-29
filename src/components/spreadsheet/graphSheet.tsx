d5a0a4e1148132212acdbd9dbc63a112e966c322
Rasika <hiwarerasika@gmail.com>
2025-12-29 12:46:42 +0530

    graphSheet

--- src/components/spreadsheet/graphSheet.tsx
+++ src/components/spreadsheet/graphSheet.tsx
@@ -1,3 +1,53 @@
+// import * as GC from "@mescius/spread-sheets";
+// import "@mescius/spread-sheets-charts";
+
+// export function setupGraphSheet(sheet: GC.Spread.Sheets.Worksheet) {
+//   sheet.name("Charts");
+
+//   sheet.setArray(0, 0, [
+//     ["Prod_Name", "Prod_Price"],
+//     ["Mouse", 250],
+//     ["Keyboard", 200],
+//     ["Pendrive", 150],
+//   ]);
+
+//   const columnChart = sheet.charts.add(
+//     "ColumnChart",
+//     GC.Spread.Sheets.Charts.ChartType.columnClustered,
+//     50,
+//     150,
+//     400,
+//     250,
+//     "A1:B4"
+//   );
+//   columnChart.title({ text: "Column Chart" });
+
+//   const lineChart = sheet.charts.add(
+//     "LineChart",
+//     GC.Spread.Sheets.Charts.ChartType.line,
+//     500,
+//     150,
+//     400,
+//     250,
+//     "A1:B4"
+//   );
+//   lineChart.title({ text: "Line Chart" });
+
+//   const pieChart = sheet.charts.add(
+//     "PieChart",
+//     GC.Spread.Sheets.Charts.ChartType.pie,
+//     950,
+//     150,
+//     400,
+//     250,
+//     "A1:B4"
+//   );
+//   pieChart.title({ text: "Pie Chart" });
+// }
+
+
+
+
 import { useEffect, useRef } from "react";
 import * as GC from "@mescius/spread-sheets";
 import "@mescius/spread-sheets-charts";
